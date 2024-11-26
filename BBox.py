653490880a6ea60f108adc0fa5bf399cd6b3460d
VoThinhVuong <127602753+VoThinhVuong@users.noreply.github.com>
2024-11-26 21:34:45 +0700

    Update to BBox and JSON handling function
    For BBox:
    - added cleaned_text, page_name, id_page, id_box and get_height().
    For BBoxes_of_JSON():
    - remove unused box base on height (< 65)

--- BBox.py
+++ BBox.py
@@ -1,6 +1,7 @@
 # @Contributors: $VoThinhVuong$
 
 import json
+import re
 
 
 class Words:
@@ -31,39 +32,69 @@ class Words:
         return (f"Word(text={self._text}, confidence={self._confidence}, "f"det_confidence={self._det_confidence}, position={self._position})")
 
 class BBox:
-    def __init__(self, box) -> None:
-        self._text = box["text"]
+    def __init__(self, box, page_name, id_page, id_box) -> None:
+        self._base_text = box["text"]
+        self._cleaned_text = re.sub(r'[^\u4E00-\u9FFF\u3400-\u4DBF\u2000-\u2A6D\u2A70-\u2EBE\uF900-\uFAFF]', '', box["text"])
         self._position = box["position"]
-        self._words = [ Words(box_word) for box_word in box["words"] ]
+        self._words = [ Words(box_word) for box_word in box["words"] if box_word["text"] in self._cleaned_text ]
+        self._page_name = page_name
+        self._id_page = f".{id_page}"
+        self._id_box = f".0{id_box}" if id_box < 10 else f".{id_box}"
 
 
     def get_position(self):
         return self._position
-
     
-    def get_text(self):
-        return self._text
+    def get_cleaned_text(self):
+        return self._cleaned_text
+    
+    def get_base_text(self):
+        return self._base_text
 
     def get_words(self):
         return self._words
     
     def get_length(self):
-        return len(self._text)
+        return len(self._cleaned_text_text)
+    
+    def get_id_box(self):
+        return self._id_box
+    
+    def get_id_page(self):
+        return self._id_page
+    
+    def get_page_name(self):
+        return self._page_name
+
+    def get_height(self):
+        return self._position[3][1] - self._position[0][1]
 
     def __repr__(self):
-        return (f"BBox(position={self._position}, text={self._text}, "
-                f"words={self._words})")
+        return (f"BBox(position={self._position}, base_text={self._base_text}, cleaned_text ={self._cleaned_text},"
+                f"page_name={self._page_name}, id_page={self._id_page}, id_box={self._id_box} ,words={self._words}),")
         
 
 # JSON phải có các giá trì từ return_position và return_choices của API
-def BBoxes_of_JSON(json_file):
+def BBoxes_of_JSON(json_file, file_name):
     try:
         data = json.loads(json_file)
     except ValueError:
         print("Không xử lý được file json.")
         return
     
-    return [ BBox(text_line) for text_line in data["data"]["text_lines"] ]
+    page_name = file_name[:7]
+    id_page = file_name[-8:-5]
+
+    result = []
+    i = 1
+    for idx, text_line in enumerate(data["data"]["text_lines"]):
+        temp = BBox(text_line,page_name,id_page, i)
+        print(temp.get_height())
+        if temp.get_height() >= 65:
+            result.append(temp)
+            i += 1
+
+    return result
 
 
 
@@ -73,12 +104,18 @@ def BBoxes_of_JSON(json_file):
 #testing
 def main():
     res = []
-    with open("example_response.json", "rb") as json_file:
-        res = BBoxes_of_JSON(json_file.read())
+    file_name = "TayDuKy_page048.json"
+    with open(file_name, "rb") as json_file:
+        res = BBoxes_of_JSON(json_file.read(), file_name)
 
     for _sentence in res:
-        print(_sentence.get_text())
+        print(_sentence.get_base_text())
+        print(_sentence.get_cleaned_text())
         print(_sentence.get_position())
+        print(_sentence.get_height())
+        print(_sentence.get_page_name())
+        print(_sentence.get_id_page())
+        print(_sentence.get_id_box())
         print("=====================")
 
 
